84f0b8919321d5b267c1f7059e87331737bc8f76
Morenkov <ivanmorenkov@mail.ru>
2017-08-29 02:03:29 +0400

    Добавьте файлы проекта.

---
+++ learner/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace learner
+{
+    public partial class Form1 : Form
+    {
+        logic log = new logic();
+        int prog;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = label2.Text = label3.Text = label4.Text = "";
+            log.N();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {}
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Text = "";
+            bool tmp = log.Next();
+            if (tmp)
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+                pictureBox1.Visible = true;
+            }
+            log.showOne(label1);
+            progressBar1.Value = (int)(log.CapasityOne() + (int)(log.CapasityTwo()));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (label1.Text != "")
+            log.showTwo(label2);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {}
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            bool tmp = log.Next2();
+            label4.Text = "";
+            if (tmp)
+            {
+                button3.Enabled = false;
+                button4.Enabled = false;
+                pictureBox2.Visible = true;
+            }
+            log.showThree(label3);
+            progressBar1.Value = (int)(log.CapasityOne() + (int)(log.CapasityTwo()));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (label3.Text != "")
+            log.showFour(label4);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ learner/logic.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace learner
+{
+    public class logic
+    {
+        public bool n1, n2;
+        Random rnd = new Random((int)DateTime.Now.Ticks);
+        Random rnd2 = new Random((int)DateTime.Now.Ticks);
+        double progress = 0.0;
+        public static char[] ch = {'*' };
+        string str, str2;
+        static string[] words = "Condensed-сгущенный*talk over-обсуждать*request-запрос *permission-разрешение *parole board-Условно-досрочное освобождение *fuss-суета *crook-мошенник *charitable-благотворительная деятельность*soar-парить*flap into-вталкивать*skim off-снимать убирать*accomplish-достигать, выполнить*wind up-заводить *chase-гнаться *arrow-стрела *destruction of property-уничтожение имущества *obtuse-тупой *scam-мошенничество * hayfield - сенокос, скошенный луг*pry smth up-подглядывать *thamp-оттиск *thick as thieves-закодычные друзья *meticulous-тщательный *buffle-пустота *foulness-гнилость *strode-шагал *spot on match- совпадение? *redemption-выкуп *intention-намерение *confused-смущённый *sober up-протрезвиться *nary-ни капли *riddle with-загадывать *coincidence-совпадение *drag-бремя *footprint-отпечаток (ноги) *fingerprint-отпечаток пальца *merit a death sentence-Заслужить смертный приговор *condone-мириться *remorsless-беспощадный *vested in me-наделил меня *life sentence-пожизненное заключение *rehabilitated-Реабилитированный *reject-отвергать *reefer-косяк с мариванной *smuggle-контробанда *jeer-насмешка *maggot-личинка *felons-преступники *blasphemy-богохульство *tough-жёсткий *delouse-выводить вшей *blink of an eye-мнгновение ока *con-вор (на доверии) *nitwit-глупый *ankles-лодыжки *straw-солома *deprive of-лишать чего-то *malfunction-неисправность *lullaby -колыбельная *infirmary-лазарет *tier-уровень *ripe-спелый *sloppy kiss-неряшливый поцелуй *bastard-ублюдок (внебрачный сын)*mica-слюда *shale-сланец  *qualify-Квалифицировать *gem shop-Магазин драгоценных камней *shoelace-шнурок *snobby-чванливый (хвастливый, надменный) *stroll-прогулка *shield-щит  *fetch smb up-привлекать *resurface-всплывать на поверхность *sweepstake-тотализатор (пари на скачках) *pester-приставать *hamstring-подколенное сухожилие *magnanimous-великодушный *hunkered-чокнутый *soapstone-мыльный камень, стеастит *pebble-галька *crowbar-лом *rapture-восторг *salvation-спасение *".Split(ch , StringSplitOptions.RemoveEmptyEntries);
+        static string[] words2 = words;
+        int[] list = new int[words.Length];
+        int[] list2 = new int[words2.Length];
+
+        public void showOne(Label lab)
+        {
+            lab.Text = W(1);
+        }
+        public void showTwo(Label lab)
+        {
+            lab.Text = W(2);
+        }
+        public void showThree(Label lab)
+        {
+            lab.Text = W2(3);
+        }
+        public void showFour(Label lab)
+        {
+            lab.Text = W2(4);
+        }
+
+        public bool Next()
+        {
+            bool check = true;
+            while (check)
+            {
+                int tmp = rnd.Next(0, words.Length);
+                if (Cont(tmp, list))
+                {
+                    if (isFull(list))
+                    {
+                        n1 = true;
+                        return true;    
+                    }
+
+                    continue;
+                }
+                list[tmp] = 1;
+                if (tmp == 0)
+                    list[tmp] = 1;
+                check = false;
+                str = words[tmp];
+            }
+            return false;
+        }
+
+        public bool Next2()
+        {
+            while (true)
+            {
+                int tmp = rnd2.Next(0, words2.Length);
+                if (Cont(tmp, list2))
+                {
+                    if (isFull(list2))
+                    {
+                        n2 = true;
+                        return true;
+                    }
+                    continue;
+                }
+                list2[tmp] = 1;
+                if (tmp == 0)
+                    list2[tmp] = 1;
+
+                str2 = words2[tmp];
+                return false;
+            }
+        }
+        internal string W(int n)
+        {
+            if (n == 1)
+            {
+                return str.Split('-')[0];
+            }
+            else if ( n == 2)
+            {
+                return str.Split('-')[1];
+            }
+            return " ";
+        }
+
+        internal string W2(int n)
+        {
+            if (n == 3)
+            {
+                return str2.Split('-')[1];
+            }
+            else if (n == 4)
+            {
+                return str2.Split('-')[0];
+            }
+            return " ";
+        }
+
+        internal bool Cont(int n, int[] arr)
+        {
+            return arr[n] == 1;
+        }
+
+        internal void N()
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+                list[i] = -1;
+            }
+            for (int i = 0; i < list2.Length; i++)
+            {
+                list2[i] = -1;
+            }
+        }
+        internal bool isFull(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == -1)
+                    return false;
+            }
+            return true;
+        }
+        public double CapasityOne()
+        {
+            double cap = 0;
+            for (int i = 0; i < list.Length; i++)
+            {
+                if (list[i] != -1)
+                    cap++;
+            }
+            return cap;
+        }
+        public double CapasityTwo()
+        {
+            double cap = 0;
+            for (int i = 0; i < list2.Length; i++)
+            {
+                if (list2[i] != -1)
+                    cap++;
+            }
+            return cap;
+        }
+
+    }
+}
